8be2aa0100dee8ca1554594c7d7a63c764cb3f61
Pierre Genthon <pierre.genthon@wildcodeschool.fr>
2020-06-04 23:44:35 +0200

    tmp

--- index.js
+++ index.js
@@ -4,63 +4,8 @@ const _ = require('lodash');
 const app = express();
 const uniqid = require('uniqid');
 const moment = require('moment');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const Joi = require('@hapi/joi');
 const PORT = process.env.PORT || 5000;
-
-const swaggerOptions = {
-  definition: {
-    swagger: '2.0',
-    info: {
-      title: 'Tasks API',
-      version: '1.0.0',
-      description: 'A simple REST API allowing to manage tasks',
-      contact: {
-        name: 'API Support',
-        url: 'https://github.com/ComicScrip',
-        email: 'pierre.genthon@wildcodeschool.com'
-      },
-      license: {
-        name: 'GNU General Public License v3.0',
-        url: 'https://www.gnu.org/licenses/gpl-3.0.en.html'
-      },
-      servers: ['http://localhost:' + PORT]
-    }
-  },
-  apis: ['./app.js']
-};
-
-/**
- * @swagger
- *
- * definitions:
- *   TaskEditableAttributes:
- *     type: object
- *     properties:
- *       name:
- *         type: string
- *         description: Represents the thing to do
- *       done:
- *         type: boolean
- *         description: Indicates whether or not the task is finished
- *   Task:
- *     allOf:
- *       - $ref: '#/definitions/TaskEditableAttributes'
- *       - type: object
- *         required:
- *           - id
- *           - createdAt
- *           - name
- *           - done
- *         properties:
- *           id:
- *             type: string
- *             description: The task's unique identifier
- *           createdAt:
- *             type: string
- *             format: date-time
- *             description: The task's creation time on the server
- */
 
 const initialTasks = [
   {
@@ -82,102 +27,109 @@ const initialTasks = [
     createdAt: moment().format()
   }
 ];
-let tasks = initialTasks;
-const setupOptions = {
-  customCss: '.swagger-ui .scheme-container { display: none }'
-};
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, setupOptions));
+
+const taskSchema = Joi.object().keys({
+  name: Joi.string().alphanum().min(1).max(30),
+  done: Joi.bool().optional(),
+});
+
+const tasksByApiKey = {};
+const apiKeys = [];
+
 app.use(cors());
 app.use(express.json());
 
+const MAX_API_KEYS = 10000;
+const MAX_TASK_PER_SESSION = 10;
+
 // To simulate latency
 const GET_TASKS_DELAY_MS = 0;
+const POST_TASK_DELAY_MS = 0;
 const PATCH_TASK_DELAY_MS = 0;
+const DELETE_TASK_DELAY_MS = 0;
 
-/**
- * @swagger
- * /tasks:
- *   get:
- *     tags:
- *       - Tasks
- *     summary: Lists tasks
- *     description: Returns all tasks
- *     produces:
- *      - application/json
- *     responses:
- *       200:
- *         description: tasks
- *         schema:
- *           type: array
- *           items:
- *             $ref: '#/definitions/Task'
- */
-app.get('/tasks', (req, res) => {
+app.get('/API_KEY', (req, res) => {
+  const newApiKey = uniqid();
+  apiKeys.push(newApiKey);
+  if (apiKeys.length > MAX_API_KEYS) {
+    const firstApiKey = apiKeys.shift();
+    delete tasksByApiKey[firstApiKey];
+  }
+  tasksByApiKey[newApiKey] = [...initialTasks];
+  res.send(newApiKey);
+})
+
+const injectSessionTasks = (req, res, next) => {
+  const {API_KEY} = req.params;
+  if(!API_KEY || !apiKeys.includes(API_KEY)) {
+    res.status(401);
+    return res.json({ errorMessage: 'You have to provide a valid API key in the url. Go to /API_KEY to get one'});
+  } else {
+    req.tasks = tasksByApiKey[req.params.API_KEY];
+    next();
+  }
+}
+
+const router = express.Router();
+router.param('API_KEY', injectSessionTasks);
+app.use(router);
+
+router.post('/:API_KEY/tasks', (req, res) => {
   setTimeout(() => {
+    const {tasks} = req
+    const { name, done } = req.body;
+
+    const {error: validationErrors} = taskSchema.validate({name, done}, { abortEarly: false, presence: 'required' });
+    if (validationErrors) {
+      res.status(400);
+      return res.json({ errorMessage: 'provided attributes aren\'t valid', validationErrors });
+    }
+
+    const existingTask = _.find(tasks, { name: name.toLowerCase() });
+    if (existingTask) {
+      res.status(400);
+      return res.json({ errorMessage: `A task named "${name.toLowerCase()}" already exists on the server` });
+    }
+
+    const newTask = { id: uniqid(), name: name.toLowerCase(), done: !!done, createdAt: moment().format() };
+    tasks.push(newTask);
+    // to avoid too much memory use since the API is public
+    if (tasks.length > MAX_TASK_PER_SESSION) {
+      tasks.shift();
+    }
+    res.status(201);
+    res.json(newTask);
+  }, POST_TASK_DELAY_MS);
+});
+
+router.get('/:API_KEY/tasks', (req, res) => {
+  setTimeout(() => {
+    const {tasks} = req
     res.json(tasks);
   }, GET_TASKS_DELAY_MS);
 });
 
-/**
- * @swagger
- *
- * /tasks/{id}:
- *   patch:
- *     tags:
- *       - Tasks
- *     summary: Updates a task
- *     description: Updates a task and return it
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: task
- *         description: Task object
- *         in:  body
- *         required: true
- *         type: string
- *         schema:
- *           $ref: '#/definitions/TaskEditableAttributes'
- *       - name: id
- *         in: path
- *         type: string
- *         required: true
- *         description: Identifier of the task to update
- *     responses:
- *       200:
- *         description: The updated Task
- *         schema:
- *           $ref: '#/definitions/Task'
- *       400:
- *         description: Invalid parameters
- *         schema:
- *           type: object
- *           properties:
- *             errorMessage:
- *               type: string
- *               description: General error message
- *             validationErrors:
- *               type: object
- *               description: Specific errors by field name
- *       404:
- *         description: Task not found
- */
-app.patch('/tasks/:id', (req, res) => {
+const mergeWhenDefined = (propsToMergeIn, target) => {
+  Object.keys(propsToMergeIn).forEach(key => {
+    if(propsToMergeIn[key] !== undefined) target[key] = propsToMergeIn[key];
+  })
+}
+
+router.patch('/:API_KEY/tasks/:id', (req, res) => {
   setTimeout(() => {
+    const {tasks} = req
+    const { done, name } = req.body;
     const { id } = req.params;
-    const { done } = req.body;
 
-    const validationErrorsByFieldName = {};
-    if (!(typeof done === 'boolean')) validationErrorsByFieldName.done = 'Must be a boolean value';
-
-    if (Object.keys(validationErrorsByFieldName).length > 0) {
+    const {error: validationErrors} = taskSchema.validate({name, done}, { abortEarly: false });
+    if (validationErrors) {
       res.status(400);
-      return res.json({ errorMessage: 'provided attributes aren\'t valid', validationErrors: validationErrorsByFieldName });
+      return res.json({ errorMessage: 'provided attributes aren\'t valid', validationErrors });
     }
-
+    
     const existingTask = _.find(tasks, { id });
     if (existingTask) {
-      existingTask.done = done;
+      mergeWhenDefined({name, done}, existingTask)
       res.json(existingTask);
     } else {
       res.sendStatus(404);
@@ -185,5 +137,20 @@ app.patch('/tasks/:id', (req, res) => {
   }, PATCH_TASK_DELAY_MS);
 });
 
+router.delete('/:API_KEY/tasks/:id', (req, res) => {
+  setTimeout(() => {
+    const {tasks} = req
+    const { id } = req.params;
+    const existingTaskIndex = _.findIndex(tasks, { id });
+    if (existingTaskIndex >= 0) {
+      tasks.splice(existingTaskIndex, 1);
+      res.sendStatus(204);
+    } else {
+      res.sendStatus(404);
+    }
+  }, DELETE_TASK_DELAY_MS);
+});
+
+app.get('/', (req, res) => res.redirect('/API_KEY'));
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
-app.get('/', (req, res) => res.redirect('/tasks'));
